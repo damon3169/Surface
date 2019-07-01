c66be1aa678a88ddf7f8b1b41db0546a5c5f5090
Unknown <ambjo@hotmail.fr>
2019-07-01 16:01:50 +0200

    Cell And player begenning

---
+++ Surface/Assets/Scripts/CellController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CellController : MonoBehaviour
+{
+
+	float timerForDoubleClick = 0.0f;
+	float delay = 0.3f;
+	bool isDoubleClick = false;
+	enum stateCell { reveal, hide};
+	stateCell cellState;
+	public Sprite hideCellSprite;
+	public Sprite revealCellSprite;
+	public PlayerController submarine;
+
+
+	void Update()
+	{
+		if (isDoubleClick == true)
+		{
+			timerForDoubleClick += Time.deltaTime;
+		}
+
+		if (timerForDoubleClick >= delay)
+		{
+			timerForDoubleClick = 0.0f;
+			isDoubleClick = false;
+		}
+
+	}
+
+
+	void OnMouseOver()
+	{
+		if (Input.GetButtonDown("Fire1") && isDoubleClick == false)
+		{
+			Debug.Log("Mouse clicked once");
+			isDoubleClick = true;
+		}
+	}
+
+	void OnMouseDown()
+	{
+		// si player a cote de this
+
+		if (isDoubleClick == true && timerForDoubleClick < delay && submarine.nearCellList.Contains(this) && !submarine.isMovingToCell)
+		{
+			//Faire se deplacer le player et ouvrir popup
+			cellState = stateCell.reveal;
+			
+			CellStateChange();
+
+			submarine.MoveToCell(this.gameObject);
+		}
+
+		// si player PAS a cote de this mais deja passer par la
+		else if (isDoubleClick == true && timerForDoubleClick < delay && cellState == stateCell.reveal)
+		{
+			//Faire revenir popup
+		}
+	}
+
+	void CellStateChange()
+	{
+		Sprite cellSprite =  this.GetComponent<SpriteRenderer>().sprite;
+		switch (cellState)
+		{
+			case stateCell.reveal:
+				{
+					cellSprite = revealCellSprite;
+					break;
+				}
+			case stateCell.hide:
+				{
+					cellSprite = hideCellSprite;
+					break;
+				}
+			default: break;
+		}
+	}
+
+}
---
+++ Surface/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public List<CellController> nearCellList;
+	private int numberBattery = 0;
+	public bool isMovingToCell = false;
+	private Vector3 cellTargetPosition;
+	public float timeTakenDuringLerp = 1f;
+	public CellController currentCell;
+
+	/// <summary>
+	/// How far the object should move when 'space' is pressed
+	/// </summary>
+	private float distanceToMove = 10;
+
+	//Whether we are currently interpolating or not
+
+	//The start and finish positions for the interpolation
+	private Vector3 _startPosition;
+	private Vector3 _endPosition;
+
+	//The Time.time value when we started the interpolation
+	private float timeStartedLerping;
+
+
+	public void MoveToCell(GameObject newCell)
+	{
+		cellTargetPosition = newCell.transform.position;
+		isMovingToCell = true;
+		timeStartedLerping = Time.time;
+		_startPosition = this.transform.position;
+		distanceToMove = Mathf.Abs(Vector3.Distance(_startPosition, cellTargetPosition));
+		currentCell = newCell.GetComponent<CellController>();
+	}
+
+	public void addBattery()
+	{
+		numberBattery += 1;
+	}
+
+	public int GetNumberBatteries()
+	{
+		return numberBattery;
+	}
+
+
+	void FixedUpdate()
+	{
+		if (isMovingToCell)
+		{
+			//We want percentage = 0.0 when Time.time = _timeStartedLerping
+			//and percentage = 1.0 when Time.time = _timeStartedLerping + timeTakenDuringLerp
+			//In other words, we want to know what percentage of "timeTakenDuringLerp" the value
+			//"Time.time - _timeStartedLerping" is.
+			float timeSinceStarted = Time.time - timeStartedLerping;
+			float percentageComplete = timeSinceStarted / timeTakenDuringLerp;
+
+			//Perform the actual lerping.  Notice that the first two parameters will always be the same
+			//throughout a single lerp-processs (ie. they won't change until we hit the space-bar again
+			//to start another lerp)
+			transform.position = Vector3.Lerp(_startPosition, cellTargetPosition, percentageComplete);
+
+			//When we've completed the lerp, we set _isLerping to false
+			if (percentageComplete >= 1.0f)
+			{
+				foundCloseCell();
+				isMovingToCell = false;
+			}
+		}
+	}
+
+	void foundCloseCell()
+	{
+		//fonction change les cells a proche contenu dans closeCellList
+	}
+}
