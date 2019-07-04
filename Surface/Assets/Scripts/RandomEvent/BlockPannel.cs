5249d85c2413c2b009d30b8898e85a69577d2828
Unknown <ambjo@hotmail.fr>
2019-07-04 16:11:02 +0200

    Add the room block random event

---
+++ Surface/Assets/Scripts/RandomEvent/BlockPannel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BlockPannel : MutePanel
+{
+
+	public TextMeshProUGUI textDescription;
+	protected override void Awake()
+	{
+		base.Awake();
+		System.Random rand = new System.Random();
+		int randomNum = rand.Next(1, PlayerPrefs.GetInt("numberCase") + 1);
+		textDescription.text = "La salle " + randomNum + " est bloquer." ;
+	}
+}
